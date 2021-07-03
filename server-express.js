c1f7de914712e1bb5fe8445753ede80063e663c9
Vikas Naik <vikasnaikpro@gmail.com>
2021-07-04 01:11:50 +0530

    setup postgresql and inserted data

--- server-express.js
+++ server-express.js
@@ -1,15 +1,48 @@
 'use strict'
 
 const express = require('express');
+const cors = require('cors');
+const {Pool} = require('pg');
+
+const db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }    
+});
+
 const app = express();
+
+app.use(cors()); 
+
 const port = process.env.PORT || 3001;
 const sessions = require('./sessions.json');
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hello Salesforce Devs from Enpress</h1>');
 });
-app.get('/api/sessions',(req,res)=>{
-    res.json(sessions);
+app.get('/api/sessions', async (req,res)=>{
+    //res.json(sessions);
+    
+    const {rows} = await db.query(`SELECT json_build_object(
+        'id', t.id,
+        'name', t.name,
+        'description', t.description,
+        'room', t.room,
+        'dateTime', t."datetime",
+        'speakers', json_agg(json_build_object(
+          'id', s.id,
+          'name', s.name,
+          'bio', s.bio,
+          'pictureUrl', s.pictureurl
+          ))
+        ) AS session
+      FROM sessions t
+      INNER JOIN sessions_speakers ts ON ts.session_id = t.id
+      INNER JOIN speakers s ON ts.speaker_id = s.id
+      GROUP BY t.id;`);
+      
+    res.json(rows);
 });
  
 app.listen(port,()=>{
